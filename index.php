27cceaac653cb585ff2c933d0d8790201877a4d3
taricatarica <bruno.tarica@estudiante.ceibal.edu.uy>
2021-11-15 00:36:17 -0300

    Ajustes en el index

--- index.php
+++ index.php
@@ -2,18 +2,12 @@
     include "head.php"; 
     include "navbar.php";
 ?>
-<br>
-<br>
-<h2 class="indigo-text center-align">¡Bienvenido a la tienda online!</h2>
-<br>
-<br>
-<h2 class="indigo-text center-align">Comencemos:</h2>
-<br>
-<div class="center">
-    <a href="/ecommerce/products/store.php" >
-        <button id="store" name="store" class="waves-effect waves-light btn indigo">Ver catálogo</button>
-    </a>
-
+<div class="container index-container">
+    <h2 class="indigo-text center-align">¡Bienvenido a la tienda online!</h2>
+    <div class="center mb-2 mt-2">
+        <a href="/ecommerce/products/store.php" >
+            <button id="store" name="store" class="waves-effect waves-light btn indigo">Ver catálogo</button>
+        </a>
+    </div>
 </div>
-
 <?php include "footer.php"; ?>
